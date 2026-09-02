0ddb9e5bfddb860e970e69880ad5c3edc2048650
Akhi <akhileshwagare1102@gmail.com>
2026-09-02 20:46:47 +0530

    Cloud Storage, PDF-Image Conversion, Key-Value Storage and AI Feedback

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -21,22 +21,23 @@ export default function Home() {
     if (!auth.isAuthenticated) navigate('/auth?next=/');
   }, [auth.isAuthenticated])
 
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    <section className="main-section">
-      <div className="page-heading py-16">
-        <h1>Track your Application & Resume Ratings</h1>
-        <h2>Review Your Submissions and check AI-Powered feedback.</h2>
-      </div>
-
-      {resumes.length > 0 && (
-        <div className="resumes-section">
-          {resumes.map(resume => (
-            <ResumeCard key={resume.id} resume={resume} />
-          ))}
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your Application & Resume Ratings</h1>
+          <h2>Review Your Submissions and check AI-Powered feedback.</h2>
         </div>
-      )}
-    </section>
 
-  </main>;
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map(resume => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
